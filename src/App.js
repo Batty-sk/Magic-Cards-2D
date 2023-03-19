cfe7b8bf78ee94fbda49799bbce8a3aa4e4c9e80
batty sk <saorav.skumar@gmail.com>
2023-03-19 19:00:23 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import Home from './Pages/Home'
+import Footer from './Components/Footer';
+function App() {
+  return (
+    <>
+    <Home></Home>
+    <Footer></Footer>
+
+    </>
+
+  );
+}
+
+export default App;
